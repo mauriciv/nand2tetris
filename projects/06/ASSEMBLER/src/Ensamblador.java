51d2a55d8fe27fc333f198db71c13f0643382b55
Mauricio <mauri.banger@gmail.com>
2016-02-18 00:18:45 -0300

    Implementado el loop basico para leer todo el archivo asm

--- projects/06/ASSEMBLER/src/Ensamblador.java
+++ projects/06/ASSEMBLER/src/Ensamblador.java
@@ -1,11 +1,15 @@
+import java.io.IOException;
 
 public class Ensamblador {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		if (args.length != 0){
 			Parser parser = new Parser(args[0]);
-			System.out.println(parser.lineaActual);
+			while (parser.hasMoreCommands()){
+				System.out.println(parser.lineaActual);
+				parser.advance();
+			}
 		}
 		else{
 			System.out.println("Error. No se especifico ningun archivo.");
